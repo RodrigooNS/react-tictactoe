63e34b0291717563926910aa204438569a8677cb
RodrigoNS <rodrigoons@gmail.com>
2024-04-18 17:08:22 -0300

    criada função onclick e menu lateral

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -6,16 +6,37 @@ import React from "react"
 
 function Game () {
   const [gameState, setGameState] = useState(Array(9).fill(0))
+  const [currentPlayer, setCurrentPlayer] = useState(1)
+
+  const handleClick = (pos) => {
+    if (gameState[pos] === 0) {
+      let newGameState = [...gameState]
+      newGameState[pos] = currentPlayer
+      setCurrentPlayer(currentPlayer * -1)
+      setGameState(newGameState)
+    }
+  }
+
   return (
-    <div className={styles.game}>
-      {
-        gameState.map((value, pos) => 
-          <GameOption 
-            key={`game-option-pos-${pos}`}
-            status={value}
-          />
-        )
-      }
+    <div className={styles.gameArea}>
+      <div className={styles.game}>
+        {
+          gameState.map((value, pos) => 
+            <GameOption 
+              key={`game-option-pos-${pos}`}
+              status={value}
+              onClick={() => handleClick(pos)}
+            />
+          )
+        }
+      </div>
+      <div className={styles.menu}>
+        <p>Jogador atual:</p>
+        <GameOption 
+          key={'player'}
+          status={currentPlayer}
+        />
+      </div>
     </div>
   )
 }
