4f1fa435f8b46a2bb41d1e0f2325585b0c199ce0
RodrigoNS <rodrigoons@gmail.com>
2024-04-18 15:12:00 -0300

    criado ícone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,17 @@ import styles from "./Header.module.css"
 
 import Subtitle from "../subtitle/Subtitle"
 import Title from "../title/Title"
+import Icon from "../icon/Icon"
 
 function Header ({ children }) {
   return (
     // <h1 className={styles.header}>{children}</h1>
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
-      <Subtitle>Criado por RodrigoNS</Subtitle>
+      <Subtitle>Criado por RodrigooNS</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/RodrigooNS" />
+      </div>
     </div>
   )
 }
